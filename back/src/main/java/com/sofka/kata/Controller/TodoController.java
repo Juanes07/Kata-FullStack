05b4173bb4ead3c3e1e0047e759867cfe05d08d4
Juanes07 <juanesdm3@gmail.com>
2022-05-03 17:35:04 -0500

    Esquelo Fronted
    Se crea el esqueleto base para el fronted de la aplicacion

--- back/src/main/java/com/sofka/kata/Controller/TodoController.java
+++ back/src/main/java/com/sofka/kata/Controller/TodoController.java
@@ -3,42 +3,47 @@ package com.sofka.kata.Controller;
 import com.sofka.kata.Model.Todo;
 import com.sofka.kata.Service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@CrossOrigin
 @RestController
-@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
     @Autowired
     private TodoService todoService;
 
 
-    @GetMapping(value = "/todos")
+    @GetMapping(value = "api/todos")
     public Iterable<Todo> list() {
         return todoService.list();
     }
 
-    @PostMapping(value = "/todo")
+    @PostMapping(value = "api/todo")
     public Todo save(@RequestBody Todo todo) {
         return todoService.save(todo);
     }
 
-    @PutMapping(value = "/todo")
+    @PutMapping(value = "api/todo")
     public Todo update(@RequestBody Todo todo) {
         if (todo.getId() != null) {
             return todoService.save(todo);
         }
         throw new RuntimeException("No existe el ToDo");
-
     }
 
-    @DeleteMapping(value = "/{id}/todo")
+    @DeleteMapping(value = "api/{id}/todo")
     public void delete(@PathVariable("id") Long id) {
         todoService.delete(id);
     }
 
-    @GetMapping(value = "/{id}/todo")
+    @GetMapping(value = "api/{id}/todo")
     public Todo get(@PathVariable("id") Long id) {
         return todoService.get(id);
     }
